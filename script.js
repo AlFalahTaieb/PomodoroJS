daf9e3df3b8bfdabd32dad5b6f1c48593d25bde4
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-14 23:49:50 +0200

    🍅

---
+++ script.js
@@ -0,0 +1,40 @@
+let countdown;
+
+const timerDisplay =document.querySelector('.display__time-left')
+const endTime =document.querySelector('.display__end-time')
+
+
+function timer (seconds){
+const now= Date.now();
+const then = now + seconds *1000;
+displayTimeLeft(seconds);
+displayEndTime(then);
+countdown= setInterval(()=>{
+const secondsLeft=Math.round((then-Date.now())/1000);
+if(secondsLeft<0){
+clearInterval(countdown)
+return;
+}
+displayTimeLeft(secondsLeft)
+},1000)
+}
+
+function displayTimeLeft (seconds) {
+	console.log(seconds)
+	const minutes = Math.floor(seconds/60);
+	const remainderSeconds=seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0' :''}${
+		remainderSeconds}`;
+
+		document.title=display;
+	timerDisplay.textContent=display;
+
+
+
+}
+function displayEndTime (timestamp) {
+	const end = new Date(timestamp)
+	const hour=end.getHours();
+	const minutes=end.getMinutes();
+	endTime.textContent=`Le Timer touchera à sa fin à 🍅${hour}:${minutes}🍅`;
+}
